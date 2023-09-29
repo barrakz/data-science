8cc0f2e88139f8e10dcf04063996c8d05ab806ad
barrakz <b.rakuzy@gmail.com>
2023-09-29 07:38:04 +0200

    use sample data

--- simple.py
+++ simple.py
@@ -1,3 +1,6 @@
+from sklearn.cluster import KMeans
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
 import pandas as pd
 from faker import Faker
 import random
@@ -6,21 +9,10 @@ import random
 fake = Faker()
 
 # Tworzenie listy miast
-cities = [fake.city() for _ in range(10)]  # 10 unikalnych miast, można dostosować ilość
+# 10 unikalnych miast, można dostosować ilość
+cities = [fake.city() for _ in range(10)]
 
-# Tworzenie przykładowych danych z powtarzającymi się miastami
-data = []
-for _ in range(100):
-    name = fake.name()
-    age = random.randint(18, 65)
-    city = random.choice(cities)  # Losowe wybieranie miasta z listy
-    email = fake.email()
-    phone_number = fake.phone_number()
-    
-    data.append([name, age, city, email, phone_number])
-
-# Tworzenie DataFrame
-df = pd.DataFrame(data, columns=["Name", "Age", "City", "Email", "Phone Number"])
+df = pd.read_csv("sample_data.csv")
 
 # 1. Analiza statystyczna
 print("Średnia wieku:", df["Age"].mean())
@@ -28,7 +20,6 @@ print("Minimalny wiek:", df["Age"].min())
 print("Maksymalny wiek:", df["Age"].max())
 
 # 2. Wizualizacja danych (przy użyciu Matplotlib)
-import matplotlib.pyplot as plt
 
 plt.hist(df["Age"], bins=10, edgecolor='k')
 plt.xlabel("Wiek")
@@ -58,16 +49,15 @@ print("Dane po przetworzeniu numeru telefonu:")
 print(df)
 
 # 7. Modelowanie predykcyjne (przy użyciu regresji liniowej)
-from sklearn.linear_model import LinearRegression
 
 X = df[["Age"]]
-y = df["Phone Number"].str.len()  # Przykładowy cel predykcyjny - długość numeru telefonu
+# Przykładowy cel predykcyjny - długość numeru telefonu
+y = df["Phone Number"].str.len()
 
 model = LinearRegression()
 model.fit(X, y)
 
 # 8. Klastrowanie (przy użyciu KMeans)
-from sklearn.cluster import KMeans
 
 X = df[["Age"]]
 kmeans = KMeans(n_clusters=3)
@@ -75,33 +65,7 @@ df["Cluster"] = kmeans.fit_predict(X)
 print("Wyniki klastrowania:")
 print(df[["Age", "Cluster"]])
 
-# # 9. Analiza korelacji
-# correlation_matrix = df.corr()
-# print("Macierz korelacji:")
-# print(correlation_matrix)
 
-# 10. Eksploracja danych (np. liczba unikalnych miast)
+# 9. Eksploracja danych (np. liczba unikalnych miast)
 unique_cities = df["City"].nunique()
 print("Liczba unikalnych miast:", unique_cities)
-
-# Wykres słupkowy z statystyką miast
-city_counts = df["City"].value_counts()
-plt.figure(figsize=(10, 6))
-plt.bar(city_counts.index, city_counts.values)
-plt.xlabel("Miasto")
-plt.ylabel("Liczba Użytkowników")
-plt.title("Statystyka Miast")
-plt.xticks(rotation=45)
-plt.tight_layout()
-plt.show()
-
-# Wykresy pudełkowe wieku w poszczególnych miastach
-plt.subplot(1, 2, 2)
-plt.boxplot([df[df["City"] == city]["Age"] for city in cities], labels=cities)
-plt.xlabel("Miasto")
-plt.ylabel("Wiek")
-plt.title("Rozkład Wieku w Poszczególnych Miastach")
-plt.xticks(rotation=45)
-
-plt.tight_layout()
-plt.show()
